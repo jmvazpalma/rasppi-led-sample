6b2f1f063c181adbb5bd558b663a17fec3f641a3
jmvazpalma <jmvazpalma@gmail.com>
2022-08-08 21:31:11 +0200

    New: Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import wpi from 'node-wiring-pi';
+
+var configPin = 7;
+var configTimeout = 1000;
+
+wpi.setup('wpi');
+wpi.pinMode(configPin, OUTPUT);
+
+var isLedOn = 0;
+
+setInterval(function() {
+	isLedOn = +!isLedOn;
+
+	wpi.digitalWrite(configPin, isLedOn );
+}, configTimeout);
